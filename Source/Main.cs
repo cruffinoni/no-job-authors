9d0a98fe64f53757863713a51fc0abc6ec1bf4f8
Jelly <jellypowered@gmail.com>
2020-02-29 12:05:10 -0600

    Fixed Errors Pawns were unable to resume unfinished jobbers.

--- Source/Main.cs
+++ Source/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using HugsLib;
@@ -14,6 +15,7 @@ namespace NoJobAuthors
         public override string ModIdentifier => "NoJobAuthors";
     }
 
+
     [HarmonyPatch(typeof(WorkGiver_DoBill), "ClosestUnfinishedThingForBill")]
     public static class WorkGiver_DoBill_ClosestUnfinishedThingForBill_Patch
     {
@@ -21,19 +23,15 @@ namespace NoJobAuthors
         public static bool ClosestUnfinishedThingForBill(ref UnfinishedThing __result, Pawn pawn, Bill_ProductionWithUft bill)
         {
             bool Validator(Thing t) => !t.IsForbidden(pawn) &&
-                                       ((UnfinishedThing) t).Recipe == bill.recipe &&
-                                       ((UnfinishedThing) t).ingredients.TrueForAll(x => bill.IsFixedOrAllowedIngredient(x.def)) &&
+                                       ((UnfinishedThing)t).Recipe == bill.recipe &&
+                                       ((UnfinishedThing)t).ingredients.TrueForAll(x => bill.IsFixedOrAllowedIngredient(x.def)) &&
                                        pawn.CanReserve(t);
 
             ThingRequest thingReq = ThingRequest.ForDef(bill.recipe.unfinishedThingDef);
             TraverseParms traverseParams = TraverseParms.For(pawn, pawn.NormalMaxDanger());
 
-            __result = (UnfinishedThing)GenClosest.ClosestThingReachable(pawn.Position,
-                pawn.Map, 
-                thingReq, 
-                PathEndMode.InteractionCell,
-                traverseParams,
-                validator: Validator);
+            __result = (UnfinishedThing)GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, thingReq, PathEndMode.InteractionCell, traverseParams, validator: Validator);
+            //Log.Message("This is closest unfinished thing for bill", false);
             return false;
         }
     }
@@ -45,6 +43,7 @@ namespace NoJobAuthors
         public static bool Creator(ref Pawn __result)
         {
             __result = null;
+            Log.Message("UnfinishedThing_GetCreator Patch", false);
             return false;
         }
     }
@@ -58,6 +57,7 @@ namespace NoJobAuthors
         public static void Creator(UnfinishedThing __instance)
         {
             _creatorName(__instance) = "Everyone";
+            //Log.Message("I just set the creator to everyone", false);
         }
     }
 
@@ -68,12 +68,14 @@ namespace NoJobAuthors
         public static IEnumerable<CodeInstruction> StartOrResumeBillJob(IEnumerable<CodeInstruction> instructions)
         {
             var arr = instructions.ToArray();
+            //Log.Message("Start or resume bill patch thing", false);
             for (var index = 0; index < arr.Length; index++)
             {
                 if (arr[index + 0].opcode == OpCodes.Ldloc_S &&
                     arr[index + 1].opcode == OpCodes.Callvirt &&
                     arr[index + 2].opcode == OpCodes.Ldarg_1 &&
                     arr[index + 3].opcode == OpCodes.Bne_Un)
+                    
                 {
                     yield return new CodeInstruction(OpCodes.Nop);
                     yield return new CodeInstruction(OpCodes.Nop);
@@ -82,7 +84,8 @@ namespace NoJobAuthors
                     index += 3;
                 }
                 else
-                    yield return arr[index];
+                    //Log.Message("We made it to start or resume bill job else statement", false);
+                yield return arr[index];
             }
         }
     }
@@ -93,13 +96,14 @@ namespace NoJobAuthors
         [HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> FinishUftJob(IEnumerable<CodeInstruction> instructions)
         {
+            //Log.Message("FinishUftJob Start Patch", false);
             var arr = instructions.ToArray();
             for (var index = 0; index < arr.Length; index++)
             {
                 if (arr[index + 0].opcode == OpCodes.Ldarg_1 &&
                     arr[index + 1].opcode == OpCodes.Callvirt &&
                     arr[index + 2].opcode == OpCodes.Ldarg_0 &&
-                    arr[index + 3].opcode == OpCodes.Beq)
+                    arr[index + 3].opcode == OpCodes.Beq_S)
                 {
                     yield return new CodeInstruction(OpCodes.Nop);
                     yield return new CodeInstruction(OpCodes.Nop);
@@ -108,7 +112,8 @@ namespace NoJobAuthors
                     index += 3;
                 }
                 else
-                    yield return arr[index];
+                    //Log.Message("FinishUftJob end else Patch", false);
+                yield return arr[index];
             }
         }
     }
